7c40294747b72ffa82d69f2f0dc30f81b4ba2fb1
clowiesilubrico <clowiesilubrico@gmail.com>
2021-04-07 21:40:00 +0800

    Edit Birthday Cake Candles Function

--- src/Solutions.java
+++ src/Solutions.java
@@ -1,6 +1,27 @@
+import java.util.List;
+
 public class Solutions {
 
+    public static int birthdayCakeCandles(List<Integer> candles) {
 
+        int max = Integer.MIN_VALUE;
+        int counter = 0;
+
+        for (Integer integer : candles) {
+            if (integer > max) {
+                max = integer;
+            }
+        }
+
+        for (Integer candle : candles) {
+            if (candle == max) {
+                counter++;
+            }
+        }
+
+
+        return counter;
+    }
 
 
 }
