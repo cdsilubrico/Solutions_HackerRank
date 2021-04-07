1686999a109eacaef0f334dbc686ddcb73814178
clowiesilubrico <clowiesilubrico@gmail.com>
2021-04-07 21:39:28 +0800

    Add Main Class

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,26 @@
+import java.util.ArrayList;
+
 public class Main {
+
+    public static void main(String[] args) {
+
+        int[] ar = {5, 20, 25, 400, 1000};
+        int[] ar2 = {1, 3, 5, 7, 9};
+        int[] ar3 = {2, 4, 6, 0, -2, -4, 14};
+        ArrayList<Integer> l1 = new ArrayList<>();
+        l1.add(1);
+        l1.add(5);
+        l1.add(250);
+        l1.add(100);
+        l1.add(250);
+        //System.out.println(secondMinimum(ar));
+        //System.out.println(Arrays.toString(mergeTwoSortedArray(ar2,ar3)));
+        //plusMinus(ar3);
+        //staircase(6);
+        //miniMaxSum(ar2);
+        System.out.println(Solutions.birthdayCakeCandles(l1));
+    }
+
 
 
 }
