5b1f4bfde368c08204d26752219893f832008f59
clowiesilubrico <clowiesilubrico@gmail.com>
2021-04-08 13:28:06 +0800

    Add timeConversion Function

--- src/Solutions.java
+++ src/Solutions.java
@@ -3,7 +3,6 @@ import java.util.List;
 public class Solutions {
 
     public static int birthdayCakeCandles(List<Integer> candles) {
-
         int max = Integer.MIN_VALUE;
         int counter = 0;
 
@@ -19,9 +18,45 @@ public class Solutions {
             }
         }
 
-
         return counter;
     }
+
+    static String timeConversion(String s) {
+        String answer = "";
+        int firstTwo = Integer.parseInt(s.substring(0, 2));
+
+        if(s.contains("AM"))
+        {
+            if(firstTwo == 12)
+            {
+                answer="00";
+                answer = answer+s.substring(2,8);
+            }
+            else
+            {
+                answer = s.substring(0,8);
+            }
+
+        }
+        else if(s.contains("PM"))
+        {
+            if(firstTwo == 12)
+            {
+                answer = s.substring(0,8);
+            }
+            else
+            {
+                firstTwo+=12;
+                answer=answer+String.valueOf(firstTwo)+s.substring(2,8);
+            }
+
+        }
+
+        return answer;
+    }
+
+
+
 
 
 }
