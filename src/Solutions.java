2f225daeada719a7f95d4ad5c8520a20a78122f7
clowiesilubrico <clowiesilubrico@gmail.com>
2021-04-10 16:01:14 +0800

    Add staircase function

--- src/Solutions.java
+++ src/Solutions.java
@@ -70,6 +70,26 @@ public class Solutions {
         return totalPairs;
     }
 
+    static void staircase(int n) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 1; j <= n; j++) {
+
+                if (j < n - i) {
+
+                    System.out.print(" ");
+                } else {
+
+                    System.out.print("#");
+
+                }
+
+            }
+            System.out.println();
+        }
+
+    }
+
+
 
 
 
