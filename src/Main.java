fb1c2c81a04d69c4a5aabc85615e9966a0651b31
clowiesilubrico <clowiesilubrico@gmail.com>
2021-04-15 21:43:38 +0800

    Update Main Driver

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
 
         int[] ar = {5, 20, 25, 400, 1000};
-        int[] ar2 = {1, 3, 5, 7, 9};
+        int[] ar2 = {5,9,10,7,4};
         int[] ar3 = {2, 4, 6, 0, -2, -4, 14};
         ArrayList<Integer> l1 = new ArrayList<>();
         l1.add(1);
@@ -20,7 +20,7 @@ public class Main {
         //miniMaxSum(ar2);
         //System.out.println(Solutions.birthdayCakeCandles(l1));
         //System.out.println(Solutions.timeConversion("12:45:54PM"));
-
+        System.out.println(Solutions.divisibleSumPairs(5,2,ar2));
 
 
 
