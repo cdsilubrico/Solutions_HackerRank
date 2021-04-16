3a2bf6980ef4c798954c14b06f78d3365ebe9808
clowiesilubrico <clowiesilubrico@gmail.com>
2021-04-16 23:17:40 +0800

    Update Main Driver

--- src/Main.java
+++ src/Main.java
@@ -9,10 +9,10 @@ public class Main {
         int[] ar3 = {2, 4, 6, 0, -2, -4, 14};
         ArrayList<Integer> l1 = new ArrayList<>();
         l1.add(1);
+        l1.add(2);
+        l1.add(3);
+        l1.add(4);
         l1.add(5);
-        l1.add(250);
-        l1.add(100);
-        l1.add(250);
         //System.out.println(secondMinimum(ar));
         //System.out.println(Arrays.toString(mergeTwoSortedArray(ar2,ar3)));
         //plusMinus(ar3);
@@ -20,8 +20,8 @@ public class Main {
         //miniMaxSum(ar2);
         //System.out.println(Solutions.birthdayCakeCandles(l1));
         //System.out.println(Solutions.timeConversion("12:45:54PM"));
-        System.out.println(Solutions.divisibleSumPairs(5,2,ar2));
-
+        //System.out.println(Solutions.divisibleSumPairs(5,2,ar2));
+        System.out.println(Solutions.migratoryBirds(l1));
 
 
 
