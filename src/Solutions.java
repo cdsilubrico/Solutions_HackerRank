71a47e5f27f3cca6c5e1dbdaa6ba790e9f5f0dd3
clowiesilubrico <clowiesilubrico@gmail.com>
2021-04-27 12:28:17 +0800

    Add utopianTree function on 4/27/2021

--- src/Solutions.java
+++ src/Solutions.java
@@ -678,6 +678,23 @@ public class Solutions {
         return max*word.length();
     }
 
+    public static int utopianTree(int n) {
+        int height;
+        height = 1;
+
+        for(int i = 1; i <=n;i++)
+        {
+            if(i%2==0)
+            {
+
+                height+=1;
+            }else
+                height = height*2;
+        }
+
+        return height;
+    }
+
 
 
 
