3488b5c875274e206951603c128fce4de9b13901
clowiesilubrico <clowiesilubrico@gmail.com>
2021-05-07 22:00:33 +0800

    Update Main Driver

--- src/Main.java
+++ src/Main.java
@@ -33,6 +33,7 @@ public class Main {
         //System.out.println(Solutions.getMoneySpent(ar,ar2,60));
         //System.out.println(Solutions.circularArrayRotation(l2,2,l1));
         //System.out.println(Solutions.myAtoi("-42 asdfasfads"));
+        System.out.println(Solutions.appendAndDelete("HelloWorld","HelloStar",6));
     }
 
 
